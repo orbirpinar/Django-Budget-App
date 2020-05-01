bedad2e82055f650466875a937759c5b9aa1bdbe
orbirpinar <orbirpinar@gmail.com>
2020-05-01 05:51:04 +0300

    change secret key

--- budget/settings.py
+++ budget/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+^t_me(*@8&5p#f=d2b%1a=ii*+hk@_-k4xag1jjxwjh(1km0w'
+SECRET_KEY = os.getenv('BUDGET_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
